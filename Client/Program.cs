6af18d80cd15b01efd11f1fe8928362ff8e6e3c4
Pamfile Alex <damrosrak@gmail.com>
2022-06-18 14:11:14 +0300

    first commit

---
+++ Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Client
+{
+    class Program
+    {
+        private static bool _isRunning = true;
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Dad Joke");
+                Console.WriteLine("2. Web API Message");
+                ConsoleKeyInfo input = Console.ReadKey();
+                string output = input.Key switch
+                {
+                    ConsoleKey.D0 => Exit(),
+                    ConsoleKey.D1 => GetDadJoke(),
+                    ConsoleKey.D2 => GetApiMessage(),
+                    _ => "Not supported"
+                };
+                Console.WriteLine();
+                Console.WriteLine(output);
+                Console.ReadKey();
+            } while (_isRunning);
+        }
+
+        public static string Exit()
+        {
+            _isRunning = false;
+            return "Bye";
+        }
+
+        public static string GetDadJoke()
+        {
+            return WebUtils.GetAsyncText("https://icanhazdadjoke.com/").Result;
+        }
+
+        public static string GetApiMessage()
+        {
+            return WebUtils.GetAsyncText("https://localhost:44368/Message").Result;
+        }
+    }
+}
---
+++ Client/WebUtils.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public static class WebUtils
+    {
+        private static readonly HttpClient _httpClient;
+        private static JsonSerializerOptions Options = new()
+        {
+            PropertyNameCaseInsensitive = true
+        };
+
+        static WebUtils()
+        {
+            _httpClient = new();
+            _httpClient.DefaultRequestHeaders.Add("Accept", "text/plain");
+        }
+
+        public static async Task<T> GetAsyncJson<T>(string url)
+        {
+            string message = await GetAsyncText(url);
+            return JsonSerializer.Deserialize<T>(message, Options);
+        }
+
+        public static async Task<string> GetAsyncText(string url)
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception("Request not successful!");
+            }
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
